f7e448dd45fd434634138e1c959f072422ef9250
Harish <harish.rajkumar2022@vitstudent.ac.in>
2023-09-22 20:46:16 +0530

    Create model for requests

---
+++ models/requestModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const rescueRequestSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      require: true,
+    },
+    requiredAgencyTypes: {
+      type: [String],
+      required: true,
+    },
+    coordinates: {
+      type: [String],
+      required: true,
+    },
+    acttive: {
+      type: Boolean,
+      required: true,
+    },
+    requestedBy: {
+      type: String,
+      required: false,
+    },
+    assignedTo: {
+      type: [String],
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("RescueRequest", rescueRequestSchema);
